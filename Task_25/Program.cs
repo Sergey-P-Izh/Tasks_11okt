6f37dfe542ec7f46927e9470d71de6b5d91f183a
Sergey <u3jiom@bk.ru>
2022-10-17 22:44:33 +0400

    Решение задач

---
+++ Task_25/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Задача 25: Напишите цикл, который принимает на вход два числа (A и B)
+// и возводит число A в натуральную степень B.
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите степень: ");
+int stepen = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("number^stepen = " + Math.Pow(number, stepen));
+Console.ReadLine();
+
+//int x = 5;
+//int stepen = 4;
+//Console.WriteLine("5^4 = " + Math.Pow(x, stepen));
---
+++ Task_27/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число
+// и выдаёт сумму цифр в числе.
+// 452 -> 11
+
+int x = int.Parse(Console.ReadLine());
+int sum=0;
+for (int i=3; i>=0; i--)
+{
+    sum+=x/(int)Math.Pow(10.0,i);
+    x=x%(int)Math.Pow(10.0,i);
+}
+Console.WriteLine(sum);
